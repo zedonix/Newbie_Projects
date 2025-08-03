cae4d4192f62ef3932a43f137ff86ec608c86422
zedonarch <zedonix@proton.me>
2025-08-03 16:06:22 +0530

    aoc started

---
+++ py/aoc/2015/1.py
@@ -0,0 +1,27 @@
+def first():
+    with open("input") as fin:
+        data = fin.read()
+        positive = data.count("(")
+        negative = data.count(")")
+        total = positive - negative
+        print(total)
+
+
+def second():
+    with open("input") as fin:
+        data = fin.read()
+        basement = False
+        total = 0
+        for i in range(len(data)):
+            if data[i] == "(":
+                total += 1
+            elif data[i] == ")":
+                total -= 1
+            if total == -1:
+                basement = True
+            if basement:
+                print(i)
+                print(total)
+                break
+
+second()
---
+++ py/test.py
@@ -0,0 +1,19 @@
+furniture = ["table", "chair", "rack", "shelf"]
+
+print(furniture[0:4])
+# ['table', 'chair', 'rack', 'shelf']
+
+print(furniture[1:3])
+# ['chair', 'rack']
+
+print(furniture[0:-1])
+# ['table', 'chair', 'rack']
+
+print(furniture[:2])
+# ['table', 'chair']
+
+print(furniture[1:])
+# ['chair', 'rack', 'shelf']
+
+print(furniture[:])
+# ['table', 'chair', 'rack', 'shelf']
